38dd805a15c24dfaf17362427a5cd8343a1ec9e6
Ankur310 <ankurjeesingh310@gmail.com>
2022-05-22 14:17:22 +0530

    Customer Management Module Completed.....

---
+++ src/main/java/com/bookmycab/controller/CustomerController.java
@@ -0,0 +1,53 @@
+package com.bookmycab.controller;
+
+
+import com.bookmycab.exceptions.CustomerExceptions;
+import com.bookmycab.model.Customer;
+import com.bookmycab.repository.CustomerRepository;
+import com.bookmycab.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    CustomerRepository customerRepositry;
+
+
+    @GetMapping("/customer")
+    public List<Customer> viewCustomers(){
+        return customerService.viewCustomers();
+    }
+
+    @GetMapping("/customer/{id}")
+    public Customer viewCustomer(@PathVariable("id") Integer id) throws CustomerExceptions {
+
+        return customerService.viewCustomer(id);
+    }
+
+    @PutMapping("/customer/{id}")
+    public Customer updateCustomer(@RequestBody Customer customer, @PathVariable("id") Integer id) throws CustomerExceptions{
+        return customerService.updateCustomer(customer,id);
+    }
+
+    @PostMapping("/customer")
+    public Customer insertCustomer(Customer customer){
+        return customerService.insertCustomer(customer);
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public Customer deleteCustomer(@PathVariable("id") Integer id){
+       return customerService.deleteCustomer(id);
+    }
+
+    @GetMapping("/customer/{name}/{pass}")
+    public Customer validateCustomer(@PathVariable("user") String username, @PathVariable("pass") String password) throws CustomerExceptions{
+        return  customerService.validateCustomer(username,password);
+    }
+}
---
+++ src/main/java/com/bookmycab/model/Customer.java
@@ -0,0 +1,13 @@
+package com.bookmycab.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Customer extends User{
+
+}
