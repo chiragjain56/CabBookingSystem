8320e8991225920e78be9eaa4e6b2e5346632331
rajujai <rajujaijangra@gmail.com>
2022-05-24 14:11:07 +0530

    Exception handling - All seperate methods fixed

--- src/main/java/com/bookmycab/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bookmycab/exceptions/GlobalExceptionHandler.java
@@ -11,21 +11,51 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CabException.class)
-    public ResponseEntity<MyErrorDetails> handleCabException(CabException exp, WebRequest req) {
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<?> handleUserException(UserException exp, WebRequest req) {
 
         MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
 
         return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
-
     }
 
-    @ExceptionHandler(UserException.class)
-    public ResponseEntity<MyErrorDetails> handleUserException(UserException exp, WebRequest req) {
+    @ExceptionHandler(DriverException.class)
+    public ResponseEntity<?> handleDriverException(DriverException exp, WebRequest req) {
 
         MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
 
         return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler(CabException.class)
+    public ResponseEntity<?> handleCabException(CabException exp, WebRequest req) {
+
+        MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CustomerException.class)
+    public ResponseEntity<?> handleCustomerException(CustomerException exp, WebRequest req) {
+
+        MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TripException.class)
+    public ResponseEntity<?> handleTripException(TripException exp, WebRequest req) {
+
+        MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception exp, WebRequest req) {
+
+        MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+
+        return new ResponseEntity<>(err, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
